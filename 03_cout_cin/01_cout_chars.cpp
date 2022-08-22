8f16b30de82e65188d20d5472afa9a81566903ea
urielhaile <perenganito@gmail.com>
2022-08-22 14:35:14 -0500

    Ejemplos visto s en clase entrada y salida estánda

---
+++ 03_cout_cin/01_cout_chars.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main () {
+  std::cout << "¡!¿\n";
+  std::cout << "?Ññó" << std::endl;
+  std::cout << "fin del programa";
+  return 0;
+}
---
+++ 03_cout_cin/02_cin_ejem.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main () {
+  unsigned int edad;
+  std::cout << "Que edad tienes?" << std::endl;
+  std::cin >> edad;
+  std::cout << "Tu edad es: " << edad << std::endl;
+  return 0;
+}
