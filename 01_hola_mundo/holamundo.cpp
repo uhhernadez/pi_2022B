fa127cfee618a54a49420d0078d4bedbae5d068f
urielhaile <perenganito@gmail.com>
2022-08-22 11:58:24 -0500

    Ejemplos de las dos sesiones

--- 01_hola_mundo/holamundo.cpp
+++ 01_hola_mundo/holamundo.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main () {
-  std::cout << "Programar es bonito!" << std::endl;
+  std::cout << 
+  "Programar es bonito!" 
+  << 
+  std::endl;
+
+  std::cout << "Programar es super bonito!" << std::endl;
+  std::cout << "Programar es hiper bonito!" << std::endl;
+  std::cout << "Programar es mega bonito!" << std::endl;
 }
