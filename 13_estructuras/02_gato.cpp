2a5b11eddfad48882645a1e12a480b41f1ad3c79
urielhaile <perenganito@gmail.com>
2022-11-10 12:45:38 -0600

    Gato ascii

--- 13_estructuras/02_gato.cpp
+++ 13_estructuras/02_gato.cpp
@@ -12,38 +12,43 @@ struct Gato {
 void MuestraInformacion (string nombre, float edad, string color) {
   cout << "Nombre: " << nombre << " edad: " << edad << " color: " << color << endl;
 }
+Gato CrearGato(string nombre, float edad, string color) {
+  Gato g;
+  g.nombre = nombre;
+  g.edad = edad;
+  g.color = color;
+  return g;
+}
+
+void MostrarGato(Gato g) {
+  cout << "Nombre: " <<g.nombre<< " edad: "<< g.edad << " color: " << g.color << endl;
+}
+
+void Maulla(Gato &g) {
+  if (g.edad < 12) {
+    cout << "Miiiiaaau" << endl;
+  } else {
+    cout << "           __..--''``---....___   _..._    __         " << endl;
+    cout << " /// //_.-'    .-/ ;  `        ``<._  ``.''_ `. / // /" << endl;
+    cout << "///_.-' _..--.'_    \\                    `( ) ) // // " << endl;
+    cout << "/ (_..-' // (< _     ;_..__               ; `' / ///  " << endl;
+    cout << " / // // //  `-._,_)' // / ``--...____..-' /// / //   " << endl;
+  }
+}
+
+void Envejecer(Gato &g, float tiempo) {
+  g.edad += tiempo;
+}
 
 int main () {
   Gato gato1, gato2, gato3;
-  gato1.color = "rojo";
-
-  string nombre_gato1 = "Lea", nombre_gato2 = "Aurora", nombre_gato3 ="Delailah";  
-  float edad_gato1 = 1, edad_gato2 = 3, edad_gato3 = 3;
-  string color_gato1 = "cafe", color_gato2 = "blanco", color_gato3 = "naranja"; 
-
-  string nombres[] = {"Lea", "Aurora", "Delailah"};
-  float edades[] = {1, 3, 3};
-  string colores[] = {"cafe", "blanco", "naranja"};
-  for (int k = 0; k < 3; k++) {
-    MuestraInformacion(nombres[k], edades[k], colores[k]);
-  }
-
-
-  MuestraInformacion(nombre_gato1, edad_gato1, color_gato1);
-  MuestraInformacion(nombre_gato2, edad_gato2, color_gato2);
-  MuestraInformacion(nombre_gato3, edad_gato3, color_gato3);
-
-  /*
-  cout << "Nombre : " << nombre_gato1
-       << " edad: " << edad_gato1 
-       << " color: " << color_gato1 << endl; 
-  
-  cout << "Nombre : " << nombre_gato2
-       << " edad: " << edad_gato2 
-       << " color: " << color_gato2 << endl; 
-  
-  cout << "Nombre : " << nombre_gato3
-       << " edad: " << edad_gato3 
-       << " color: " << color_gato3 << endl; 
-  */
+  gato1 = CrearGato("Lea", 1, "cafe");
+  gato2 = CrearGato("Aurora", 3, "blanco");
+  gato3 = CrearGato("Delailah", 3, "naranja");
+  MostrarGato(gato1);
+  MostrarGato(gato2);
+  MostrarGato(gato3);
+  Envejecer(gato1, 9.5);
+  MostrarGato(gato1);
+  Maulla(gato1);
 }
